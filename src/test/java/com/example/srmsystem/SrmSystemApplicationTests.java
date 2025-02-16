cc9555e0387ae92cdfef67aa79ab2dca43160a6a
kirirut <rutkkir@gmail.com>
2025-02-16 23:34:50 +0300

    Stylechecker

---
+++ src/test/java/com/example/srmsystem/SrmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmSystemApplicationTests {
+
+    @Test
+    void contextLoads() {//Test
+    }
+
+}
