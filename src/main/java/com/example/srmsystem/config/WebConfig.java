f628a432a08bc5780b2402cd4e788ab1db1aedec
kirirut <rutkkir@gmail.com>
2025-05-19 01:20:04 +0300

    dockerfile

--- src/main/java/com/example/srmsystem/config/WebConfig.java
+++ src/main/java/com/example/srmsystem/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Разрешаем CORS для всех путей
-                .allowedOrigins("http://fastcrm.tw1.su") // Разрешаем запросы с фронтенда
+                .allowedOrigins("http://fastcrm.tw1.su:3000") // Разрешаем запросы с фронтенда
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Разрешенные HTTP-методы
                 .allowedHeaders("*") // Разрешаем все заголовки
                 .allowCredentials(true); // Разрешаем отправку куки (если нужно)
