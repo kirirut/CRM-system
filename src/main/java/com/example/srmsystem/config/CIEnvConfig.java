f422c9264f796ed604d746472e30e38f0a7bcce8
kirirut <rutkkir@gmail.com>
2025-05-19 09:11:41 +0300

    fix

--- src/main/java/com/example/srmsystem/config/CIEnvConfig.java
+++ src/main/java/com/example/srmsystem/config/CIEnvConfig.java
@@ -3,8 +3,9 @@ package com.example.srmsystem.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
 
 @Configuration
 @Profile("ci")
